0226885bd353f81ef79c5b5279e35f38705abc45
khizar43 <74181516+khizar43@users.noreply.github.com>
2026-06-03 07:34:37 +0530

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,4 +1,18 @@
-from python import greet
+from python import app
 
-def test_greet():
-    assert greet("Khizer") == "Hello Khizer"
+def test_home():
+    client = app.test_client()
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert b"Hello Khizer" in response.data
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get('/health')
+
+    assert response.status_code == 200
+    assert response.json["status"] == "UP"
