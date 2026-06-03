cdea065752b0eb8b2547a2918f5aaa58868db84f
khizar43 <74181516+khizar43@users.noreply.github.com>
2026-06-03 07:34:22 +0530

    Update python.py

--- python.py
+++ python.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello Khizer! Jenkins + Docker is working."
 
+@app.route("/health")
+def health():
+    return {"status": "UP"}
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
